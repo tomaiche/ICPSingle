9205cd25422353e8d27ce20671cf6d1db1d1748a
tomaiche <thomas_aiche@fr.ibm.com>
2018-10-16 17:13:40 +0200

    cleanup for IBMCloud

--- ibmcloud/terraform/variables.tf
+++ ibmcloud/terraform/variables.tf
@@ -1,21 +1,16 @@
 # Single Node
-variable "singlenode_hostname_ip" {
-  type    = "map"
+variable "hostname" {
+  type    = "string"
 }
 
 variable "singlenode_vcpu" {
   type    = "string"
-  default = "8"
 }
 
 variable "singlenode_memory" {
   type    = "string"
-  default = "16384"
 }
 
-variable "singlenode_vm_template" {
-  type = "string"
-}
 
 variable "singlenode_vm_os_user" {
   type = "string"
@@ -25,26 +20,12 @@ variable "singlenode_vm_os_password" {
   type = "string"
 }
 
-variable "singlenode_vm_ipv4_gateway" {
-  type = "string"
-}
-
-variable "singlenode_vm_ipv4_prefix_length" {
-  type = "string"
-}
 
 variable "singlenode_vm_disk1_size" {
   type    = "string"
   default = "400"
 }
 
-variable "singlenode_vm_disk1_datastore" {
-  type = "string"
-}
-
-variable "singlenode_vm_disk1_keep_on_remove" {
-  type    = "string"
-  default = "false"
 }
 
 variable "singlenode_vm_disk2_enable" {
@@ -57,48 +38,12 @@ variable "singlenode_vm_disk2_size" {
   default = ""
 }
 
-variable "singlenode_vm_disk2_datastore" {
-  type    = "string"
-  default = ""
-}
 
-variable "singlenode_vm_disk2_keep_on_remove" {
-  type    = "string"
-  default = "false"
-}
 
 variable "vm_domain" {
   type = "string"
 }
 
-variable "vm_network_interface_label" {
-  type = "string"
-}
-
-variable "vm_adapter_type" {
-  type    = "string"
-  default = "vmxnet3"
-}
-
-variable "vm_folder" {
-  type = "string"
-}
-
-variable "vm_dns_servers" {
-  type = "list"
-}
-
-variable "vm_dns_suffixes" {
-  type = "list"
-}
-
-variable "vsphere_datacenter" {
-  type = "string"
-}
-
-variable "vsphere_resource_pool" {
-  type = "string"
-}
 
 # SSH KEY Information
 variable "icp_private_ssh_key" {
@@ -122,12 +67,10 @@ variable "icp_binary_url" {
 
 variable "icp_version" {
   type    = "string"
-  default = "2.1.0.3"
 }
 
 variable "kub_version" {
   type    = "string"
-  default = "1.10.0"
 }
 
 variable "download_user" {
@@ -141,12 +84,10 @@ variable "download_user_password" {
 # ICP Settings
 variable "enable_kibana" {
   type    = "string"
-  default = "true"
 }
 
 variable "enable_metering" {
   type    = "string"
-  default = "true"
 }
 
 variable "icp_cluster_name" {
@@ -155,12 +96,10 @@ variable "icp_cluster_name" {
 
 variable "icp_admin_user" {
   type    = "string"
-  default = "admin"
 }
 
 variable "icp_admin_password" {
   type    = "string"
-  default = "admin"
 }
 
 variable "enable_bluemix_install" {
