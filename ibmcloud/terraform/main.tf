1a663abfb70ac8da07a45b0966436ad9053a9e6a
tomaiche <thomas_aiche@fr.ibm.com>
2018-10-17 20:38:09 +0200

    Update main.tf

--- ibmcloud/terraform/main.tf
+++ ibmcloud/terraform/main.tf
@@ -78,7 +78,7 @@ module "push_hostfile" {
   private_key          = "${length(var.icp_private_ssh_key) == 0 ? "${tls_private_key.generate.private_key_pem}" : "${var.icp_private_ssh_key}"}"
   vm_os_password       = "${var.singlenode_vm_os_password}"
   vm_os_user           = "${var.singlenode_vm_os_user}"
-  vm_ipv4_address_list = "${list("169.51.2.186", module.deployVM_singlenode.ipv4)}"
+  vm_ipv4_address_list = "${list(var.singlenode_hostname)}"
   #######
   bastion_host        = "${var.bastion_host}"
   bastion_user        = "${var.bastion_user}"
