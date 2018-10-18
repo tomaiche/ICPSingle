2bc3bda93b87d509693d8a399a95676329f7cc1e
tomaiche <thomas_aiche@fr.ibm.com>
2018-10-18 09:40:32 +0200

    isolate count var

--- ibmcloud/terraform/tom_modules/config_hostfile/config_hostfile.tf
+++ ibmcloud/terraform/tom_modules/config_hostfile/config_hostfile.tf
@@ -7,7 +7,7 @@ resource "null_resource" "master_dependsOn" {
 resource "null_resource" "generate_hostfile" {
   depends_on = ["null_resource.master_dependsOn"]
 
-  count = "${var.count)}"
+  count = "${var.host_count)}"
   connection {
     type = "ssh"
     user = "${var.vm_os_user}"
