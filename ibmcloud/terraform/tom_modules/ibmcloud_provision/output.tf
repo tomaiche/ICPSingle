704ec59c7a5ce0734434829886173b721bdab794
tomaiche <thomas_aiche@fr.ibm.com>
2018-11-08 19:52:00 +0100

    switch to Private IP

--- ibmcloud/terraform/tom_modules/ibmcloud_provision/output.tf
+++ ibmcloud/terraform/tom_modules/ibmcloud_provision/output.tf
@@ -1,2 +1,6 @@
 output "dependsOn" { value = "${null_resource.vm-create_done.id}" description="Output Parameter when Module Complete"}
-output "ipv4" { value = "${ibm_compute_vm_instance.softlayer_virtual_guest.ipv4_address}" description="IPV4 Address when Module Complete"}
+# output "ipv4" { value = "${ibm_compute_vm_instance.softlayer_virtual_guest.ipv4_address}" description="IPV4 Address when Module Complete"}
+output "ipv4" {
+                value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address_private}" 
+                description="IPV4 Address when Module Complete"
+              }
