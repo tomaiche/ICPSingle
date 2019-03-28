37a87ab6700676228290f291203e03165af864ba
tomaiche <thomas_aiche@fr.ibm.com>
2019-03-28 11:35:50 +0100

    switch to private @

--- ibmcloud/terraform/tom_modules/ibmcloud_provision/output.tf
+++ ibmcloud/terraform/tom_modules/ibmcloud_provision/output.tf
@@ -1,9 +1,10 @@
 output "dependsOn" { value = "${null_resource.vm-create_done.id}" description="Output Parameter when Module Complete"}
-output "ipv4" { value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address}" description="IPV4 Address when Module Complete"}
 /*
+  output "ipv4" { value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address}" description="IPV4 Address when Module Complete"}
+*/
 output "ipv4" {
                 value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address_private}" 
                 description="IPV4 Address when Module Complete"
               }
-*/
+
   
